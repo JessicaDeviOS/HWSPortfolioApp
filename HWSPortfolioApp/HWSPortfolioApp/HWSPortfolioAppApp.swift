e63a2e5c649dc5a16af830f40f2d82fcf9065d87
Jessica Mallian <jlmallian@gmail.com>
2022-10-17 14:06:21 -0500

    created xcode project

---
+++ HWSPortfolioApp/HWSPortfolioApp/HWSPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSPortfolioAppApp.swift
+//  HWSPortfolioApp
+//
+//  Created by Jessica Mallian on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct HWSPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
